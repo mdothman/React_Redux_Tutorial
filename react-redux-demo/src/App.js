b4e3792c4daf1f33c87036dd0141ce5c4f2e3553
BEELZ <othmanmuhidean@gmail.com>
2020-11-22 13:45:36 -0800

    The simplest react-redux patter, lesson 18

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import './App.css';
+import CakeContainer from './componenets/CakeContainer';
+
+function App() {
+  return (
+    <Provider store={store}>
+        <div className="App">
+      <CakeContainer />
+    </div>
+    </Provider>
+  
+  );
+}
+
+export default App;
