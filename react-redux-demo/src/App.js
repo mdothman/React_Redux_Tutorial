8d3ca903eeacfc42a6e90c46c8756ad689c0362a
BEELZ <othmanmuhidean@gmail.com>
2020-11-22 14:29:19 -0800

    redux using hooks for selector and dispatch

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -3,12 +3,14 @@ import {Provider} from 'react-redux';
 import store from './redux/store';
 import './App.css';
 import CakeContainer from './componenets/CakeContainer';
+import HooksCakeContainer from './componenets/hooksContainer';
 
 function App() {
   return (
     <Provider store={store}>
         <div className="App">
       <CakeContainer />
+      <HooksCakeContainer />
     </div>
     </Provider>
   
